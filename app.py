8aaac1cf0d09b3ab54bfdedaf8620e149affdf75
Giancarlo <mision.marquez1314@gmail.com>
2018-12-18 09:26:17 -0700

    fin

--- app.py
+++ app.py
@@ -11,7 +11,15 @@ def home():
 def menu():
     return render_template('menu.html')
 
-    
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
